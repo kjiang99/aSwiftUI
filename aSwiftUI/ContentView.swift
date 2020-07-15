c698cad968bb7e16d4666d818e4f1234479a9ee6
Kevin Jiang <kjiang22206@yahoo.com>
2020-07-15 13:21:58 -0400

    Initial Commit

---
+++ aSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  aSwiftUI
+//
+//  Created by Kevin Jiang on 7/15/20.
+//  Copyright © 2020 Kevin Jiang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
