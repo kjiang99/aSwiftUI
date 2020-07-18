57341dcd65a7d9ec5f828debb9efd0ac3714003f
Kevin Jiang <kjiang22206@yahoo.com>
2020-07-18 14:00:06 -0400

    Initial setup

--- aSwiftUI/ContentView.swift
+++ aSwiftUI/ContentView.swift
@@ -10,9 +10,80 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false){
+                VStack(spacing: 200) {
+                    HStack(spacing: 50) {
+                        NavigationLink(destination: MovieView()) {
+                            Text("Movie1").font(.title)
+                        }//.buttonStyle(PlainButtonStyle())
+                            .navigationBarTitle("Movie1")
+                        
+                        NavigationLink(destination: MovieView()) {
+                            Text("Movie2").font(.title)
+                        }//.buttonStyle(PlainButtonStyle())
+                            .navigationBarTitle("Move2")
+                        
+                    }
+                    
+                    HStack(spacing: 50) {
+                        NavigationLink(destination: MovieView()) {
+                            Text("Movie1").font(.title)
+                        }//.buttonStyle(PlainButtonStyle())
+                            .navigationBarTitle("Movie1")
+                        
+                        NavigationLink(destination: MovieView()) {
+                            Text("Movie2").font(.title)
+                        }//.buttonStyle(PlainButtonStyle())
+                            .navigationBarTitle("Move2")
+                        
+                    }
+                    
+                    HStack(spacing: 50) {
+                        NavigationLink(destination: MovieView()) {
+                            Text("Movie1").font(.title)
+                        }//.buttonStyle(PlainButtonStyle())
+                            .navigationBarTitle("Movie1")
+                        
+                        NavigationLink(destination: MovieView()) {
+                            Text("Movie2").font(.title)
+                        }//.buttonStyle(PlainButtonStyle())
+                            .navigationBarTitle("Move2")
+                        
+                    }
+                    
+                    
+                    HStack(spacing: 50) {
+                        NavigationLink(destination: MovieView()) {
+                            Text("Movie1").font(.title)
+                        }//.buttonStyle(PlainButtonStyle())
+                            .navigationBarTitle("Movie1")
+                        
+                        NavigationLink(destination: MovieView()) {
+                            Text("Movie2").font(.title)
+                        }//.buttonStyle(PlainButtonStyle())
+                            .navigationBarTitle("Move2")
+                        
+                    }
+                    Spacer()
+                                
+                    }.navigationBarTitle("Main Menu")
+            }
+        }
     }
 }
+
+
+//struct MenuButton: View {
+//    let title: String
+//    let view: some View
+//
+//    var body: some view {
+//        HStack {
+//            NavigationLink(destination: <#T##_#>, isActive: <#T##Binding<Bool>#>, label: <#T##() -> _#>)
+//        }
+//    }
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ aSwiftUI/Movie/MovieView.swift
@@ -0,0 +1,21 @@
+//
+//  MovieView.swift
+//  aSwiftUI
+//
+//  Created by Kevin Jiang on 7/15/20.
+//  Copyright © 2020 Kevin Jiang. All rights reserved.
+//
+
+import SwiftUI
+
+struct MovieView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieView()
+    }
+}
